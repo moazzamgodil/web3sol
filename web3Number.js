1bb9ace25615f943520db4e49ed0cc068bc327f3
Moazzam Ahmed <moazzamgodil@gmail.com>
2024-07-08 18:20:53 +0500

    Update

---
+++ web3Number.js
@@ -0,0 +1,85 @@
+const web3Divide = (val, b) => {
+    let a = val.toString();
+    let chkDec = a.split(".");
+    if (chkDec.length > 1) {
+        a = chkDec[0];
+    }
+    let len = a.length;
+    let i = 0;
+    while (i < b) {
+        len = len - 1;
+        i++;
+    }
+    let v1 = a.substring(0, len);
+    let v2 = a.substring(len, a.length);
+    if (len <= 0) {
+        let point = len * -1;
+        v1 = "0.";
+        i = 0;
+        while (i < point) {
+            v1 += "0";
+            i++;
+        }
+    } else if (Number(v2) !== 0) {
+        v1 = v1 + ".";
+    }
+    v2 = v2.split("");
+    while (true) {
+        if (v2[v2.length - 1] !== "0") {
+            break;
+        }
+        v2.pop();
+    }
+    v2 = v2.join("");
+    return v1 + v2;
+};
+
+const web3Multiply = (val, b) => {
+    const a = val.toString();
+    let v1 = a.split(".");
+    let len = v1.length > 1 ? b - v1[1].length : b;
+    let i = 0;
+    while (i < len) {
+        v1.push("0");
+        i++;
+    }
+    v1 = v1.join("");
+    const v2 = v1.split("");
+    while (true) {
+        if (v2[0] == "0") {
+            v2.shift();
+            i++;
+        } else {
+            break;
+        }
+    }
+    return v2.join("");
+};
+
+export const web3Number = (number, isBigToSmall = true, dec = 18, isLocale = null) => {
+    if (Number(number) === 0 || isNaN(Number(number))) {
+        return 0;
+    }
+    let num = number;
+    if (typeof number == "number" || typeof number == "bigint") {
+        num = num.toString();
+    }
+    if (num.includes("+")) {
+        num = Number(num).toLocaleString().replaceAll(",", "");
+    }
+    if (isBigToSmall) {
+        num = web3Divide(num, dec);
+    } else {
+        num = web3Multiply(num, dec);
+    }
+
+    if (isLocale) {
+        num = numLocale(num);
+    }
+
+    return num;
+};
+
+export const numLocale = (num) => {
+    return Number(num).toLocaleString();
+}
