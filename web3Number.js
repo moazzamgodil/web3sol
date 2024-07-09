bc5158430b925a6f942bc8949a020c4df97d8aa9
Moazzam Ahmed <moazzamgodil@gmail.com>
2024-07-09 19:21:10 +0500

    fixes

--- web3Number.js
+++ web3Number.js
@@ -1,4 +1,7 @@
 const web3Divide = (val, b) => {
+    if (b < 2) {
+        return val;
+    }
     let a = val.toString();
     let chkDec = a.split(".");
     if (chkDec.length > 1) {
@@ -35,14 +38,27 @@ const web3Divide = (val, b) => {
 };
 
 const web3Multiply = (val, b) => {
+    if (b < 2) {
+        return val;
+    }
     const a = val.toString();
     let v1 = a.split(".");
     let len = v1.length > 1 ? b - v1[1].length : b;
     let i = 0;
-    while (i < len) {
-        v1.push("0");
-        i++;
+    if (len > 0) {
+        while (i < len) {
+            v1.push("0");
+            i++;
+        }
+    } else if (len < 0) {
+        let v1_2 = v1[1].split("");
+        while (i > len) {
+            v1_2.pop();
+            i--;
+        }
+        v1[1] = v1_2.join("");
     }
+
     v1 = v1.join("");
     const v2 = v1.split("");
     while (true) {
@@ -64,19 +80,25 @@ export const web3Number = (number, isBigToSmall = true, dec = 18, isLocale = nul
     if (typeof number == "number" || typeof number == "bigint") {
         num = num.toString();
     }
-    if (num.includes("+")) {
-        num = Number(num).toLocaleString().replaceAll(",", "");
+    if (num.includes("+") || num.includes("-")) {
+        if (num.indexOf("+") != -1) {
+            num = Number(num).toLocaleString().replaceAll(",", "");
+        } else {
+            const splitNum = num.split("-");
+            const exponentNum = splitNum[splitNum.length - 1];
+            const numLen = splitNum[0].length - 1;
+            num = Number(num).toFixed(Number(exponentNum) + Number(numLen));
+        }
     }
+
     if (isBigToSmall) {
         num = web3Divide(num, dec);
     } else {
         num = web3Multiply(num, dec);
     }
-
     if (isLocale) {
         num = numLocale(num);
     }
-
     return num;
 };
 
